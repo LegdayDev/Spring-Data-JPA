59030a3abefabb531cc848336990176f2f05cdc2
Legday <chlwodud0327@naver.com>
2023-10-31 20:50:59 +0900

    Feat: 스프링 데이터 JPA 페이징
    Page : 페이징 기능을 제공하는 인터페이스 PageRequest 클래스로 페이징 조건을 파라미터로 주면 count 쿼리까지 알아서 나간다.
    주의할 점은 left outer join 쿼리가 나가면 count 쿼리도 left outer join 쿼리로 나가기 때문에 count 쿼리를 분리해야 할 경우도 생긴다.
    Slice : Page 인터페이스의 부모 인터페이스이고 다양한 기능을 제공한다. 하지만 count 쿼리는 지원하지 않는다.

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
@@ -169,9 +173,41 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findListByUsername("aaa");
         Member result2 = memberRepository.findMemberByUsername("aaa");
         Optional<Member> result3 = memberRepository.findOptionalByUsername("aaa");
-
-
     }
+
+    @Test
+    public void paging(){
+        //given
+        memberRepository.save(new Member("member1",10));
+        memberRepository.save(new Member("member2",10));
+        memberRepository.save(new Member("member3",10));
+        memberRepository.save(new Member("member4",10));
+        memberRepository.save(new Member("member5",10));
+        memberRepository.save(new Member("member6",10));
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.ASC,"username"));
+
+        //when
+        Page<Member> page = memberRepository.findByAge(age, pageRequest); // API 통신 시 Entity 자체를 반환하는건 안좋다.
+
+        Page<MemberDto> toMap = page.map(m -> new MemberDto(m.getId(), m.getUsername(), ""));
+
+        //then
+        List<Member> content = page.getContent();
+        long totalCount = page.getTotalElements();
+
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(6);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.isLast()).isFalse();
+        assertThat(page.hasNext()).isTrue();
+    }
+
+
+
 
 
 }
